b77b851ae61b912cd651ccadcdf7e28c2d4f3749
Pranav B <pranavbhuv@gmail.com>
2023-12-06 16:23:52 -0600

    doops

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,44 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:tandem/ui/login.dart';
 import 'package:tandem/ui/message.dart';
 import 'package:web_socket_channel/status.dart' as status;
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 import 'firebase_options.dart';
+
+void sendPostRequest() async {
+  final ipAddress = '3417389189';
+  final port = 3000;
+  final endpoint = '/initsession';
+  final url = Uri.http('$ipAddress:$port', endpoint);
+
+  final Map<String, dynamic> requestData = {
+    // Add any data you want to send in the request body here
+    // For example:
+    // 'key1': 'value1',
+    // 'key2': 'value2',
+  };
+
+  final response = await http.post(
+    url,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(requestData), // Encode data as JSON
+  );
+
+  if (response.statusCode == 200) {
+    print('Response data: ${response.body}');
+  } else {
+    print('Request failed with status code: ${response.statusCode}');
+    print('Response data: ${response.body}');
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +46,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  sendPostRequest();
 
   final wsUrl = Uri.parse('ws://localhost:1234');
   var channel = WebSocketChannel.connect(wsUrl);
